32c33ce4db7dc553e8167a3d3a4b7729fae80b9f
Khaled Alamin <55840512+KhaledAlamin@users.noreply.github.com>
2021-01-25 16:57:42 +0100

    Update caltech_dataset.py

--- caltech_dataset.py
+++ caltech_dataset.py
@@ -1,59 +1,78 @@
 from torchvision.datasets import VisionDataset
+
 from PIL import Image
-import numpy as np
+
 import os
 import os.path
 import sys
 from typing import Any, Callable, cast, Dict, List, Optional, Tuple
-from collections import OrderedDict
+import re
+import numpy as np
+
 
 def pil_loader(path):
     # open path as file to avoid ResourceWarning (https://github.com/python-pillow/Pillow/issues/835)
     with open(path, 'rb') as f:
         img = Image.open(f)
         return img.convert('RGB')
-    
-def train_test_split(split):
-    image=[]
-    label_name=[]
 
-    if split == "train":
-        im_folder = np.loadtxt('Caltech101/train.txt', dtype=str)   
-    elif split == "test":
-        im_folder = np.loadtxt('Caltech101/test.txt', dtype=str)
-
-    for i in range(0, len(im_folder)):
-        y = im_folder[i].split('/')
-        if y[0] != "BACKGROUND_Google":
-            label_name.append(y[0])
-            image.append(y[1])
-    return image, label_name
 
 
 class Caltech(VisionDataset):
     def __init__(self, root, split='train', transform=None, target_transform=None):
-
         super(Caltech, self).__init__(root, transform=transform, target_transform=target_transform)
 
-        self.root = root
-        self.split = split 
-        image, label_name = train_test_split(self.split)                             
-        label_ind = [{val : key for key, val in enumerate(OrderedDict.fromkeys(label_name))}[ele] for ele in label_name]
+        self.split = split # This defines the split you are going to use
+                           # (split files are called 'train.txt' and 'test.txt')
+        label = []
+        image = []
+        target = []
+        c, k = 0, 0
+        if split == 'train':
+            fdata = np.loadtxt('Caltech101/train.txt', dtype=str)
+        else:
+            fdata = np.loadtxt('Caltech101/test.txt', dtype=str)
+        for i in range(0, len(fdata)):
+            y = fdata[i].split('/')
+            if y[0] != 'BACKGROUND_Google':
+                target.append(y[0])
+                image.append(y[1])
+                label.append(c)
+                if (target[k] != target[k-1]) & (k > 0):
+                    c+=1
+                k+=1
 
-        self.label_name = label_name
-        self.label_ind = label_ind
         self.image = image
-        
-    def __getitem__(self, index):
-        path = os.path.join(self.root, self.label_name[index], self.image[index])
-        label = self.label_ind[index]
-        image = pil_loader(path) 
+        self.label = label
+        self.target = target
 
-        # Applies preprocessing when accessing the image:
+       
+    def __getitem__(self, index):
+        path = os.path.join(self.root, self.target[index], self.image[index])
+        '''
+        __getitem__ should access an element through its index
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (sample, target) where target is class_index of the target class.
+        '''
+        image = pil_loader(path) # Provide a way to access image and label via index
+                           # Image should be a PIL Image
+                           # label can be int
+
+        # Applies preprocessing when accessing the image
         if self.transform is not None:
             image = self.transform(image)
+        label = torch.from_numpy(np.asarray(self.label[index]))
+        label = label.long()
         return image, label
 
     def __len__(self):
-        length = len(self.image)
+
+        '''
+        The __len__ method returns the length of the dataset
+        It is mandatory, as this is used by several other components
+        '''
+        length = len(self.label)
         return length
+    
